45060bae928cc4e56a1f6b3482bdffdfbc8fe8b8
unknown <kostyafan2006@gmail.com>
2025-05-15 02:39:09 +0700

    login logout

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,11 +5,12 @@ use Model\Post;
 use Model\User;
 use Src\Request;
 use Src\View;
+use Src\Auth\Auth;
 class Site
 {
-    public function index(Request $request): string
+    public function index(): string
     {
-        $posts = Post::where('id', $request->id)->get();
+        $posts = Post::all();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
@@ -21,9 +22,26 @@ class Site
     public function signup(Request $request): string
     {
         if ($request->method==='POST' && User::create($request->all())){
-            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+            app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 
 }
